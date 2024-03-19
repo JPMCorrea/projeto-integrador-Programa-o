ca85ca960a302cbf7f1c4b8352cec74a2e930c30
JPMCorrea <161458180+JPMCorrea@users.noreply.github.com>
2024-03-19 10:24:57 -0300

    Add files via upload

---
+++ Trabalho 6/PYTHON 2. ty.py
@@ -0,0 +1,41 @@
+# Definindo variáveis para armazenar 4 números
+numero1 = 10
+numero2 = 20
+numero3 = 30
+numero4 = 40
+
+# Definindo uma variável para armazenar uma frase
+frase = "Python é uma linguagem de programação poderosa e versátil."
+
+# Definindo uma variável para armazenar uma palavra
+palavra = "programação"
+
+# Calculando a média aritmética entre os quatro números
+media = (numero1 + numero2 + numero3 + numero4) / 4
+
+# Calculando o quadrado de um dos números
+quadrado_numero = numero1 ** 2
+
+# Calculando o dobro de um dos números
+dobro_numero = numero2 * 2
+
+# Calculando a quantidade de letras da palavra
+quantidade_letras_palavra = len(palavra)
+
+# Calculando a quantidade de espaços em branco da frase
+quantidade_espacos = frase.count(' ')
+
+# Verificando se o primeiro número é maior que o segundo
+primeiro_maior_que_segundo = numero1 > numero2
+
+# Encontrando o maior número entre os quatro
+maior_numero = max(numero1, numero2, numero3, numero4)
+
+# Imprimindo os resultados
+print("Média aritmética:", media)
+print("Quadrado de um número:", quadrado_numero)
+print("Dobro de um número:", dobro_numero)
+print("Quantidade de letras da palavra:", quantidade_letras_palavra)
+print("Quantidade de espaços em branco da frase:", quantidade_espacos)
+print("O primeiro número é maior que o segundo?", primeiro_maior_que_segundo)
+print("O maior número é:", maior_numero)
